bb0c7424d55e7f43f7d06c2a4b5ac95271e62e7d
David Chavez <david_12-95@hotmail.com>
2018-03-22 10:08:43 -0700

    Add project files.

---
+++ Efecto/Delay.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace Efecto
+{
+    class Delay : ISampleProvider
+    {
+        private ISampleProvider fuente;
+
+        int offsetTiempoMS;
+
+        public int OffsetTiempoMS
+        {
+            get
+            {
+                return OffsetTiempoMS;
+            }
+            set
+            {
+                offsetTiempoMS = value;
+            }
+        }
+
+        List<float> muestras = new List<float>();
+
+        public Delay(ISampleProvider fuente)
+        {
+            this.fuente = fuente;
+            offsetTiempoMS = 500;
+            //50ms - 5000ms
+        }
+
+        public Delay(ISampleProvider fuente, float offsetTimepoMS)
+        {
+            this.fuente = fuente;
+            OffsetTiempoMS = offsetTiempoMS;
+            if (offsetTiempoMS > 5000)
+                OffsetTiempoMS = 5000;
+            else if (offsetTiempoMS < 50)
+                OffsetTiempoMS = 50;
+        }
+
+        public WaveFormat WaveFormat
+        {
+            get
+            {
+                return fuente.WaveFormat;
+            }
+        }
+
+        //Offset es el numero de muestras leidas
+        public int Read(float[] buffer, int offset, int count)
+        {
+            //Calculo de tiempo
+            var read = fuente.Read(buffer, offset, count);
+            float tiempoTranscurrido = (float)muestras.Count / (float)fuente.WaveFormat.SampleRate;
+            int muestrasTranscurridas = muestras.Count;
+            float tiempoTranscurridoMS = tiempoTranscurrido * 1000;
+            int numMuestrasOffsetTiempo = (int)(((float)offsetTiempoMS / 1000.0f) * (float)fuente.WaveFormat.SampleRate);
+
+            //Añadir muestras
+            for (int i = 0; i < read; i++)
+            {
+                muestras.Add(buffer[i]);
+            }
+
+            //Modificar muestras
+            if (tiempoTranscurridoMS > offsetTiempoMS)
+            {
+                for (int i = 0; i < read; i++)
+                {
+                    buffer[i] += muestras[muestrasTranscurridas + i - numMuestrasOffsetTiempo];
+                }
+            }
+
+            return read;
+        }
+    }
+}
---
+++ Efecto/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Win32;
+
+using NAudio.Wave;
+
+namespace Efecto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WaveOutEvent waveOut;
+        AudioFileReader reader;
+        Efecto1 efectoProvider;
+        Delay delayProvider;
+        DelayAtenuado delayAtenuadoProvider;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            waveOut = new WaveOutEvent();
+        }
+
+        private void btnExaminar_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if ((bool) fileDialog.ShowDialog())
+            {
+                txtRuta.Text = fileDialog.FileName;
+                reader = new AudioFileReader(fileDialog.FileName);
+            }
+        }
+
+        private void btnReproducir_Click(object sender, RoutedEventArgs e)
+        {
+            if (waveOut != null)
+            {
+                if (waveOut.PlaybackState == PlaybackState.Playing)
+                {
+                    waveOut.Stop();
+                }
+                waveOut.Init(reader);
+                waveOut.Play();
+                sldFactor.IsEnabled = true;
+            }
+
+        }
+
+        private void btnEfecto_Click(object sender, RoutedEventArgs e)
+        {
+            if (waveOut != null && reader != null)
+            {
+                if (waveOut.PlaybackState == PlaybackState.Playing)
+                {
+                    waveOut.Stop();
+                }
+                efectoProvider = new Efecto1(reader, (float)sldFactor.Value);
+                waveOut.Init(efectoProvider);
+                waveOut.Play();
+            }
+        }
+
+        private void sldFactor_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (efectoProvider != null)
+            {
+                efectoProvider.Factor = (float)sldFactor.Value;
+            }
+        }
+
+        private void btnDelay_Click(object sender, RoutedEventArgs e)
+        {
+            if (waveOut != null && reader != null)
+            {
+                if (waveOut.PlaybackState == PlaybackState.Playing)
+                {
+                    waveOut.Stop();
+                }
+                delayProvider = new Delay(reader);
+                waveOut.Init(delayProvider);
+                waveOut.Play();
+            }
+        }
+
+        private void sldDelay_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (waveOut != null && reader != null)
+            {
+                delayAtenuadoProvider.OffsetTiempoMS = (int)sldDelay.Value; ;
+            }
+        }
+
+        private void btnDelayAtenuado_Click(object sender, RoutedEventArgs e)
+        {
+            if (waveOut != null && reader != null)
+            {
+                if (waveOut.PlaybackState == PlaybackState.Playing)
+                {
+                    waveOut.Stop();
+                }
+                delayAtenuadoProvider = new DelayAtenuado(reader, (int)sldDelay.Value, (float)sldAtenuante.Value);
+                waveOut.Init(delayAtenuadoProvider);
+                waveOut.Play();
+                sldAtenuante.IsEnabled = true;
+                sldDelay.IsEnabled = true;
+            }
+        }
+
+        private void sldAtenuante_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (waveOut != null && reader != null)
+            {
+                delayAtenuadoProvider.Factor = (float)sldAtenuante.Value; ;
+            }
+        }
+    }
+}
